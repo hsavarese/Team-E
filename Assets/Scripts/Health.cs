a303ae15aeb2810b711ad62255f10bfe21ff3c2e
Renzopuff <lorenzo.hayes@maine.edu>
2025-04-15 16:31:29 -0400

    fixed the bug with I-frames lasting forever

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -23,20 +23,24 @@ public class Health : MonoBehaviour
     void Update() //no matter what damage kills the enemy, the enemy will drop bio bits
     {
         if (healthPoints <= 0)
-{
-    if (isEnemy)
-    {
-        BasicEnemy enemy = GetComponent<BasicEnemy>();
-        if (enemy != null)
         {
-            enemy.Die(); 
-            return; // prevent further after destruction
-        }
-    }
+            if (isEnemy)
+            {
+                BasicEnemy enemy = GetComponent<BasicEnemy>();
+                if (enemy != null)
+                {
+                    enemy.Die(); 
+                    return; // prevent further after destruction
+                }
+    
+            }
 
-    Destroy(this.gameObject); // fallback if no enemy logic
-    return;
-}
+            Destroy(this.gameObject); // fallback if no enemy logic
+            return;
+        }
+        if(invulTimer > 0){
+            invulTimer -= Time.deltaTime;
+        }
 
     }
     /*{
