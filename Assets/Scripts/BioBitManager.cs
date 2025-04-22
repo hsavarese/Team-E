df98c02a74b34943d397a86f7952685f3c6cabbd
hsavarese <huntersavarese135@gmail.com>
2025-04-22 19:55:49 -0400

    saves bio bits to the player

--- Assets/Scripts/BioBitManager.cs
+++ Assets/Scripts/BioBitManager.cs
@@ -7,16 +7,27 @@ public class BioBitManager : MonoBehaviour
 
     public int bioBits = 0;
     public Text bioBitText;
+    private const string SAVE_KEY = "BioBits";
 
     void Awake()
     {
-        Instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            LoadBioBits();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void AddBits(int amount)
     {
         bioBits += amount;
         UpdateUI();
+        SaveBioBits();
     }
 
     void UpdateUI()
@@ -24,5 +35,24 @@ public class BioBitManager : MonoBehaviour
         if (bioBitText != null)
             bioBitText.text = "BIO Bits: " + bioBits.ToString();
     }
+
+    private void SaveBioBits()
+    {
+        PlayerPrefs.SetInt(SAVE_KEY, bioBits);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadBioBits()
+    {
+        bioBits = PlayerPrefs.GetInt(SAVE_KEY, 0);
+        UpdateUI();
+    }
+
+    /*public void ResetBits() incase you need to reset them
+    {
+        PlayerPrefs.DeleteKey(SAVE_KEY);
+        bioBits = 0;
+        UpdateUI();
+    } */
 }
 
